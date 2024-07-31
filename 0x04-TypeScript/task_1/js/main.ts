e39f50cee7c1359d52066810d183ba46fcd2965b
wangui <wanguilindsey@gmail.com>
2024-08-01 00:00:45 +0300

    updates

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,36 +1,47 @@
-/*interface Teacher {
-    firstName: string,
-    lastName: string,
-    location: string,
-    fullTimeEmployee: true | false,
-    yearsOfExperience?: number,
-    contract?: any
-}
-interface Directors extends Teacher {
-    numberOfReports: number
-}*/
-
-interface user {
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+  }
+  
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: printTeacherFunction = function (
     firstName: string,
     lastName: string
-}
-
-class StudentClass {
+  ): string {
+    return `${firstName[0]}. ${lastName}`;
+  };
+  
+  class StudentClass implements StudentInterface {
+    public firstName: string;
+    public lastName: string;
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    public workOnHomework(): string {
+      return "Currently working";
+    }
+  
+    public displayName(): string {
+      return `${this.firstName}`;
+    }
+  }
+  
+  interface StudentInterface {
     firstName: string;
     lastName: string;
-
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName,
-        this.lastName = lastName
-    }
-
-    workOnHomework() {
-        console.log("Currently working")
-        return ("Currently working")
-    }
-
-    displayName() {
-        console.log(this.firstName)
-        return this.firstName;
-    }
-}
+    workOnHomework(): string;
+    displayName(): string;
+  }
