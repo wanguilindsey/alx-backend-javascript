61a5b2b8d8f9eb5fdfd1661da2bc7d1d78230f75
wanguilindsey <wanguilindsey@gmail.com>
2024-09-17 15:22:59 -0700

    message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Logs a message to the console.
+ * 
+ * @param {string} msg - The message to log.
+ */
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+module.exports = displayMessage;
